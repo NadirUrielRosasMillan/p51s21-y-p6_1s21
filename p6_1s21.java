73c655f8a2cde01a20be67f36cde0098caa8c432
Nadir Uriel Rosas Millan <144179674+NadirUrielRosasMillan@users.noreply.github.com>
2023-09-13 11:00:12 -0600

    Add files via upload

---
+++ p6_1s21.java
@@ -0,0 +1,11 @@
+public class p6_1s21 {
+    public static void main(String[] args) {
+        String nombre = "victor";
+        int edad = 23;
+
+        System.out.println("el valor de el nombre es " + nombre);
+        System.out.println("la edad es " + edad);
+        System.out.println("puede seguir programando <3");
+
+    }
+}
